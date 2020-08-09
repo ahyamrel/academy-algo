5db95823ae740d3f84c5767ad1bf25c3cde9df9e
Marik <borodkinmariel@gmail.com>
2020-08-09 13:05:31 +0300

    c#

---
+++ 1/Shlos1/Shlos1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shlos1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
