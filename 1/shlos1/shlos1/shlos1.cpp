b52ba9bae2837a37ecc00e5a55917fb11db3cfdb
Marik <borodkinmariel@gmail.com>
2020-08-15 02:02:49 +0300

    Part 1

--- 1/shlos1/shlos1/shlos1.cpp
+++ 1/shlos1/shlos1/shlos1.cpp
@@ -1,14 +1,100 @@
-// shlos1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+int BigCross1(int** matrix, int sideLen);
+
+#define SIDELEN 15
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Initialize matrix
+	int** matrix = new int* [SIDELEN];
+	for (int i = 0; i < SIDELEN; ++i)
+	{
+		matrix[i] = new int[SIDELEN];
+	}
+
+	// Initialize random array
+	for (int row = 0; row < SIDELEN; row++)
+	{
+		for (int col = 0; col < SIDELEN; col++)
+		{
+			matrix[row][col] = rand() % 2;
+		}
+	}
+
+	// Print array
+	for (int row = 0; row < SIDELEN; row++)
+	{
+		for (int col = 0; col < SIDELEN; col++)
+		{
+			cout << matrix[row][col] << " ";
+		}
+		cout << endl;
+	}
+
+	BigCross1(matrix, SIDELEN);
+	int a = 1;
 }
 
+/* Part 1
+	O(m^3)=O(n^(1.5)) 
+*/
+int BigCross1(int** matrix, int sideLen)
+{
+	bool bCrossGood = true;
+	int crossMidRow = 0;
+	int crossMidCol = 0;
+	int crossLen = 0;
+	int maxCrossLen = 0;
 
+	for (int row = 1; row < sideLen - 1; row++)
+	{
+		for (int col = 1; col < sideLen - 1; col++)
+		{
+			if (matrix[row][col] == 1)
+			{
+				while (bCrossGood)
+				{
+					crossLen++;
+
+					// Check if we don't overflow
+					if (!(row - crossLen == -1 || row + crossLen == sideLen || col - crossLen == -1 || col + crossLen == sideLen))
+					{
+
+						// Check if any side has 0 on it, if it does the cross ends 
+						if (matrix[row - crossLen][col] == 0 || matrix[row + crossLen][col] == 0 || matrix[row][col + crossLen] == 0 || matrix[row][col - crossLen] == 0)
+						{
+							bCrossGood = false;
+						}
+					}
+					else
+					{
+						bCrossGood = false;
+					}
+
+				}
+
+				// times 2 because it's double sided and add 1 because the middle doesn't count in the crossLen
+				if (maxCrossLen < (crossLen - 1) * 2 + 1)
+				{
+					maxCrossLen = (crossLen - 1) * 2 + 1;
+					crossMidRow = row;
+					crossMidCol = col;
+				}
+
+				// Reset flag and crossLen
+				bCrossGood = true;
+				crossLen = 0;
+			}
+		}
+	}
+
+	// return biggest cross length
+	// crossMidRow and crossMidCol Hold the cross position
+	return maxCrossLen;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
